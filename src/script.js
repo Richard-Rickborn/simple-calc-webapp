b1d565285f8f3bd7fce7ad82b074e281755d3411
Richard-Rickborn <44210678+Richard-Rickborn@users.noreply.github.com>
2019-02-05 18:28:44 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,19 +5,52 @@ let displayWindow = document.getElementById("display-text");
 let displayingResult = false;
 updateDisplay();
 
+document.getElementById("btn-7").addEventListener("click",function(){updateExpression("7");});
 
+document.getElementById("btn-plus").addEventListener("click",function(){updateExpression("+");});
 
+document.getElementById("btn-equals").addEventListener("click",function(){resolveExpression();});
+
+let btn_table = {
+	"btn-7":[function(){updateExpression("7");}],
+	"":[],
+	"":[]
+};
+
+/*Object.keys(obj).forEach(function (key) {
+  var value = obj[key];
+  
+  Use to iterate through the btn_table above
+  on page load to setup event listeners;
+});*/
 
 function updateExpression(str){
 	currentExpression += str;
+	
+	if(displayingResult){
+		displayingResult = false;
+	}
+	
+	if(expResult != ""){
+		expResult = "";
+	}
+	
+	updateDisplay();
+	console.log("updateExpression called: " + str);
 }
 
 function resolveExpression(){
-	expResult = eval(currentExpression).toString();
-	displayingResult = true;
-	updateDisplay();
-	//previousCommand = //the last operation and number in the string, i.e. "1+1+2+7" would end up with a previousCommand of "+7"
-	currentExpression = "";
+	if(!displayingResult){
+		expResult = eval(currentExpression).toString();
+		displayingResult = true;
+		updateDisplay();
+		//previousCommand = //the last operation and number in the string, i.e. "1+1+2+7" would end up with a previousCommand of "+7"
+		currentExpression = "";
+	} else{
+		currentExpression = expResult + "+37"; //temp value until I can get lastOperation saving working
+		displayingResult = false;
+		resolveExpression();
+	}
 }
 
 function updateDisplay(){
@@ -32,12 +65,12 @@ function updateDisplay(){
 	}
 }
 
-updateExpression("1");
-updateExpression("+");
-updateExpression("2");
-updateExpression("*");
-updateExpression("100000000");
-
-
-resolveExpression();
-console.log(expResult);
+function runTestExpression(){
+	updateExpression("1");
+	updateExpression("+");
+	updateExpression("2");
+	updateExpression("*");
+	updateExpression("100000000");
+	resolveExpression();
+	console.log(expResult);
+}
