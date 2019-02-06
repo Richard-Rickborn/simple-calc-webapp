5c70f6811fb5397c0d6042434e4728e4ebd050c0
Richard-Rickborn <44210678+Richard-Rickborn@users.noreply.github.com>
2019-02-05 19:23:34 -0500

    numpad buttons working

--- src/script.js
+++ src/script.js
@@ -5,24 +5,33 @@ let displayWindow = document.getElementById("display-text");
 let displayingResult = false;
 updateDisplay();
 
-document.getElementById("btn-7").addEventListener("click",function(){updateExpression("7");});
+//clean this up later vv
+/*document.getElementById("btn-7").addEventListener("click",function(){updateExpression("7");});
 
 document.getElementById("btn-plus").addEventListener("click",function(){updateExpression("+");});
 
-document.getElementById("btn-equals").addEventListener("click",function(){resolveExpression();});
+document.getElementById("btn-equals").addEventListener("click",function(){resolveExpression();});*/
+//clean this up later ^^
 
 let btn_table = {
 	"btn-7":[function(){updateExpression("7");}],
-	"":[],
-	"":[]
+	"btn-8":[function(){updateExpression("8");}],
+	"btn-9":[function(){updateExpression("9");}],
+	"btn-4":[function(){updateExpression("4");}],
+	"btn-5":[function(){updateExpression("5");}],
+	"btn-6":[function(){updateExpression("6");}],
+	"btn-1":[function(){updateExpression("1");}],
+	"btn-2":[function(){updateExpression("2");}],
+	"btn-3":[function(){updateExpression("3");}],
+	"btn-0":[function(){updateExpression("0");}],
+	"btn-00":[function(){updateExpression("00");}],
+	"btn-dot":[function(){updateExpression(".");}],
+	"btn-plus":[function(){updateExpression("+");}],
+	"btn-equals":[function(){resolveExpression();}]
 };
 
-/*Object.keys(obj).forEach(function (key) {
-  var value = obj[key];
-  
-  Use to iterate through the btn_table above
-  on page load to setup event listeners;
-});*/
+Object.keys(btn_table).forEach(function(element){document.getElementById(element).addEventListener("click",btn_table[element][0]);});
+//pretty sure that's the longest line of javascript I've written so far, just saying
 
 function updateExpression(str){
 	currentExpression += str;
@@ -73,4 +82,4 @@ function runTestExpression(){
 	updateExpression("100000000");
 	resolveExpression();
 	console.log(expResult);
-}
+}  //don't really need this function anymore, maybe clean this up soon
