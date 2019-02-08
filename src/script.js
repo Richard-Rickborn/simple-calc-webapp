d918cd8a7009c834dad2a8410afc12284e0ef9c9
Richard-Rickborn <44210678+Richard-Rickborn@users.noreply.github.com>
2019-02-07 22:36:35 -0500

    created Calculation class

--- src/script.js
+++ src/script.js
@@ -6,6 +6,91 @@ let displayingResult = false;
 let powerON = true;
 updateDisplay();
 
+class Calculation {
+	
+	constructor(){
+		this.nums = [];
+		this.ops = [];
+		this.currentExp = "";//change to currentExpression later
+		this.currentNumber = "";
+	}
+	
+	addNum(){
+		//add a check that currentNumber is a legit number
+		this.nums.push(this.currentNumber);
+		this.currentNumber = "";
+	}
+	
+	addOp(op){
+		this.addNum();
+		this.ops.push(op);
+	}
+	
+	updateCurrentNumber(num){
+		this.currentNumber = this.currentNumber.concat(num);
+	}
+	
+	toString(){
+		let exprString = "";
+		for(let i = 0; i < this.nums.length; i++){
+			if(this.ops[i] != "^"){
+				exprString = exprString + this.nums[i];
+				if(i < this.ops.length){
+					exprString = exprString + this.ops[i];
+				}
+			} else {
+				if(i < this.nums.length - 2){
+					exprString = exprString + "Math.pow(" + this.nums[i] + "," + this.nums[i+1] + ")";
+					i = i + 1;
+				} else{
+					exprString = exprString + "Math.pow(" + this.nums[i] + ",1)";
+				}
+			}
+		console.log(i + ":  " + exprString);
+		}
+		if(exprString != ""){
+			return exprString;
+		} else{
+			//throw an error?
+			return "0";
+		}
+	}
+}
+
+let testCalc = new Calculation();
+
+testCalc.updateCurrentNumber("2");
+testCalc.updateCurrentNumber("2");
+
+testCalc.addOp("+");
+
+testCalc.updateCurrentNumber("3");
+
+testCalc.addOp("*");
+
+testCalc.updateCurrentNumber("5");
+
+testCalc.addOp("-");
+
+testCalc.updateCurrentNumber("7");
+
+testCalc.addOp("^");
+
+//testCalc.updateCurrentNumber("3");
+
+testCalc.addNum();
+//this last addNum() would be run automatically when you pressed equals
+
+console.log(testCalc.nums);
+console.log(testCalc.ops);
+console.log(testCalc.currentNumber === "");
+//^^ so far this is CONFIRMED working.  Now, once I get the toString() working, I should be able to implement this into the actual calculator and clean up a lot of code in this file.
+
+console.log("Final result:  '" + testCalc.toString() + "'");
+//LATEST UPDATE:
+/* The toString() function appears to be working.  The next step is to integrate the Calculation class into the actual calculator functionality and do a bunch of tests to make sure strings are being generated properly, then add in eval() functionality with equals and make sure order of operations are being observed
+*/
+
 let btn_table = {
 	"btn-ON":[function(){turnOnPower();}],
 	"btn-OFF":[function(){turnOffPower();}],
